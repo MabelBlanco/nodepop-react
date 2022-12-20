c8e40f739914bbe9834960300eeee3d53fe01792
Mabel <cimne7@gmail.com>
2022-12-20 21:56:06 +0100

    pass location to navigate in loginPage

--- src/components/common/RequireAuth.js
+++ src/components/common/RequireAuth.js
@@ -1,11 +1,13 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export function RequireAuth({ children }) {
   const { isLogged } = useAuth();
 
+  const location = useLocation();
+
   if (!isLogged) {
-    return <Navigate to="/login"></Navigate>;
+    return <Navigate to="/login" state={{ from: location }}></Navigate>;
   } else {
     return children;
   }
